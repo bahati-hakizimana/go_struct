b82a84680d62939f9b1fa3ea18317522efee63b6
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2024-04-04 04:39:59 -0700

    Go_Struct logic

--- main.go
+++ main.go
@@ -6,4 +6,18 @@ func main() {
 
 	fmt.Println("Welcome to Struct Phase")
 
+	bahati := User{"bahati", "bahati@gmail.com", true, 24}
+
+    fmt.Println(bahati)
+	fmt.Printf("my details are: %+v\n", bahati)
+
+}
+
+
+
+type User struct {
+	Name string
+	Email string
+	Status bool
+	Age int
 }
